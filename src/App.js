c3156363dad0b052088a14c9beb2830176763fa1
hannahmnixon <90346288+hannahmnixon@users.noreply.github.com>
2021-11-09 17:14:21 -0800

    working out alert kinks

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello!</h1>
-        <Weather />
+        <Weather city="San Francisco" />
       </header>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,12 @@
 import React from "react";
 import axios from "axios";
 
-export default function Weather() {
+export default function Weather(props) {
   function handleResponse(response) {
-    alert(`The weather in San Francisco is ${response.data.main.temp}°C`);
+    alert("alert");
   }
   let apiKey = "a6ff4b7a9b12bc3ba444e702f94356c3";
-  let apiUrl = `api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=${apiKey}&units=metric`;
+  let apiUrl = `api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=${apiKey}`;
 
   axios.get(apiUrl).then(handleResponse);
   return <h2>Hello from Weather</h2>;
