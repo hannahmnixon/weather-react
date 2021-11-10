46c1e02c2904fa9d6d76796f1f03e1fc55dedddd
hannahmnixon <90346288+hannahmnixon@users.noreply.github.com>
2021-11-09 17:16:51 -0800

    fixed alert

--- src/Weather.js
+++ src/Weather.js
@@ -3,10 +3,9 @@ import axios from "axios";
 
 export default function Weather(props) {
   function handleResponse(response) {
-    alert("alert");
+    alert(`The weather in San Francisco is ${response.data.main.temp}°C`);
   }
-  let apiKey = "a6ff4b7a9b12bc3ba444e702f94356c3";
-  let apiUrl = `api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=${apiKey}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=San%20Francisco&appid=a6ff4b7a9b12bc3ba444e702f94356c3&units=metric`;
 
   axios.get(apiUrl).then(handleResponse);
   return <h2>Hello from Weather</h2>;
